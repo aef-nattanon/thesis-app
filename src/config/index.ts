ccada6b1b728b124996c12578aeccbd94996afe5
aefnattanon <aef.nattanon@gmail.com>
2023-01-17 20:13:01 +0700

    Update env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 const config = {
-  THESIS_API_URL: process.env.THESIS_API_URL || "https://thesis-api-mz65meldjq-as.a.run.app",
+  THESIS_API_URL:
+    process.env.THESIS_API_URL || "https://thesis-api-z3gsvhr6fa-as.a.run.app",
 };
 
 export default config;
