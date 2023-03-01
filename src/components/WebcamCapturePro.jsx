fa7a81784b20f3ee77ea04227f82c3bef8e130b6
aefnattanon <aef.nattanon@gmail.com>
2023-03-02 03:44:23 +0700

    Update files

--- src/components/WebcamCapturePro.jsx
+++ src/components/WebcamCapturePro.jsx
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
-import Camera from 'react-html5-camera-photo';
+import React from 'react';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 
-import ImagePreview from './ImagePreview'; // source code : ./src/demo/AppWithImagePreview/ImagePreview
-
-function App (props) {
-  const [dataUri, setDataUri] = useState('');
-
-  function handleTakePhotoAnimationDone (dataUri) {
-    console.log('takePhoto');
-    setDataUri(dataUri);
+function App({ setImage}) {
+  function handleTakePhoto (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto2');
+    setImage(dataUri)
   }
 
-  const isFullscreen = false;
+  function handleTakePhotoAnimationDone (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto1');
+  }
+
+  function handleCameraError (error) {
+    console.log('handleCameraError', error);
+  }
+
+  function handleCameraStart (stream) {
+    console.log('handleCameraStart');
+  }
+
+  function handleCameraStop () {
+    console.log('handleCameraStop');
+  }
+
   return (
-    <div>
-      {
-        (dataUri)
-          ? <ImagePreview dataUri={dataUri}
-            isFullscreen={isFullscreen}
-          />
-          : <Camera onTakePhotoAnimationDone = {handleTakePhotoAnimationDone}
-            isFullscreen={isFullscreen}
-          />
-      }
-    </div>
+    <Camera
+      onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+      onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
+      onCameraError = { (error) => { handleCameraError(error); } }
+      idealFacingMode = {FACING_MODES.ENVIRONMENT}
+      idealResolution = {{width: 640, height: 480}}
+      imageType = {IMAGE_TYPES.JPG}
+      imageCompression = {0.97}
+      isMaxResolution = {true}
+      isImageMirror = {false}
+      isSilentMode = {false}
+      isDisplayStartCameraError = {true}
+      isFullscreen = {false}
+      sizeFactor = {1}
+      onCameraStart = { (stream) => { handleCameraStart(stream); } }
+      onCameraStop = { () => { handleCameraStop(); } }
+    />
   );
 }
 
