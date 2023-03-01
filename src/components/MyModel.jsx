e42f5b34785031107cb976b566794fb861841ff8
aefnattanon <aef.nattanon@gmail.com>
2023-03-02 06:17:40 +0700

    Add new code

--- src/components/MyModel.jsx
+++ src/components/MyModel.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from "react";
 import { Col, Row } from 'antd';
-const tf = require('@tensorflow/tfjs');
+import * as tf from "@tensorflow/tfjs";
 
 // const weights = 'http://aef-nattanon.github.io/web_model/model.json';
 const weights = '/web_model/model.json';
@@ -10,6 +10,7 @@ const names = ['meter', 'modle 1', 'model 2']
 function MeterModel({image}) {
 
   const [model, setModel] = useState(null);
+  const [meterText, setMeterText] = useState('');
 
   useEffect(() => {
     tf.loadGraphModel(weights).then(model => {
@@ -22,13 +23,14 @@ function MeterModel({image}) {
     const naturalWidth = image.naturalWidth;
     const naturalHeight = image.naturalHeight;
 
-    // canvas.width = image.width;
-    // canvas.height = image.height;
+    canvas.width = image.width;
+    canvas.height = image.height;
 
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     const ratio = Math.min(canvas.width / image.naturalWidth, canvas.height / image.naturalHeight);
+    console.log('-----ratio', ratio)
     const newWidth = Math.round(naturalWidth * ratio);
     const newHeight = Math.round(naturalHeight * ratio);
     ctx.drawImage(
@@ -45,11 +47,36 @@ function MeterModel({image}) {
 
   };
 
+  const cropImage = (image, canvas, ctx, [x1, y1, x2, y2]) => {
+
+    const naturalWidth = image.naturalWidth;
+    const naturalHeight = image.naturalHeight;
+    x1 *= naturalWidth;
+    x2 *= naturalWidth;
+    y1 *= naturalHeight;
+    y2 *= naturalHeight;
+    const width = x2 - x1;
+    const height = y2 - y1;
+
+    ctx.drawImage(
+      image,
+      x1, y1,
+      width,
+      height,
+      0,
+      0,
+      width,
+      height,
+    );
+  }
+
   const onImageChange = e => {
     const c = document.getElementById("canvas");
+    const c2 = document.getElementById("canvas2");
     const ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.width, c.height);
+    const ctx2 = c2.getContext("2d");
     cropToCanvas(e.target, c, ctx);
+    console.log('1 crop', e.target)
     let [modelWidth, modelHeight] = model.inputs[0].shape.slice(1, 3);
     const input = tf.tidy(() => {
       return tf.image.resizeBilinear(tf.browser.fromPixels(c), [modelWidth, modelHeight])
@@ -57,6 +84,7 @@ function MeterModel({image}) {
     });
     model.executeAsync(input).then(res => {
       // Font options.
+      console.log('2 model.executeAsync', res)
       const font = "16px sans-serif";
       ctx.font = font;
       ctx.textBaseline = "top";
@@ -67,11 +95,26 @@ function MeterModel({image}) {
       const classes_data = classes.dataSync();
       const valid_detections_data = valid_detections.dataSync()[0];
 
+      console.log('3 datas')
+      console.log('boxes_data: ', boxes_data)
+      console.log('scores_data: ', scores_data)
+      console.log('classes_data: ', classes_data)
+      console.log('valid_detections_data: ', valid_detections_data)
+      console.log('---------------')
       tf.dispose(res)
-
       var i;
       for (i = 0; i < valid_detections_data; ++i){
+
+        console.log('3 for', )
         let [x1, y1, x2, y2] = boxes_data.slice(i * 4, (i + 1) * 4);
+
+
+
+        // cropImage(c, c2, ctx2, [x1, y1, x2, y2]);
+
+
+
+
         x1 *= c.width;
         x2 *= c.width;
         y1 *= c.height;
@@ -83,28 +126,43 @@ function MeterModel({image}) {
 
         // Draw the bounding box.
         ctx.strokeStyle = "#00FFFF";
-        ctx.lineWidth = 4;
+        ctx.lineWidth = 1;
         ctx.strokeRect(x1, y1, width, height);
+        setMeterText(klass + " : " + score)
 
-        // Draw the label background.
-        ctx.fillStyle = "#00FFFF";
-        const textWidth = ctx.measureText(klass + ":" + score).width;
-        const textHeight = parseInt(font, 10); // base 10
-        ctx.fillRect(x1, y1, textWidth + 4, textHeight + 4);
+
+
+        ctx2.drawImage(
+          c,
+          x1, y1,
+          width,
+          height,
+          0,
+          0,
+          width,
+          height,
+        );
+
+
+        // // Draw the label background.
+        // ctx.fillStyle = "#00FFFF";
+        // const textWidth = ctx.measureText(klass + " : " + score).width;
+        // const textHeight = parseInt(font, 10); // base 10
+        // ctx.fillRect(x1, y1, textWidth + 4, textHeight + 4);
 
       }
-      for (i = 0; i < valid_detections_data; ++i){
-        let [x1, y1, , ] = boxes_data.slice(i * 4, (i + 1) * 4);
-        x1 *= c.width;
-        y1 *= c.height;
-        const klass = names[classes_data[i]];
-        const score = scores_data[i].toFixed(2);
+      // for (i = 0; i < valid_detections_data; ++i){
+      //   let [x1, y1, , ] = boxes_data.slice(i * 4, (i + 1) * 4);
+      //   x1 *= c.width;
+      //   y1 *= c.height;
+      //   const klass = names[classes_data[i]];
+      //   const score = scores_data[i].toFixed(2);
 
-        // Draw the text last to ensure it's on top.
-        ctx.fillStyle = "#000000";
-        ctx.fillText(klass + ":" + score, x1, y1);
+      //   // Draw the text last to ensure it's on top.
+      //   ctx.fillStyle = "#000000";
+      //   ctx.fillText(klass + " : " + score, x1, y1);
 
-      }
+      // }
     });
   };
 
@@ -124,7 +182,11 @@ function MeterModel({image}) {
           </Col>
 
           <Col>
-            <canvas id="canvas" />
+            <canvas id="canvas" className="Dropzone-img" />
+            <p>{ meterText}</p>
+          </Col>
+          <Col>
+            <canvas id="canvas2" />
           </Col>
         </>
       ) : (
--- src/components/WebcamCapturePro.jsx
+++ src/components/WebcamCapturePro.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Button, Row,Col } from 'antd';
 import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 
 function App({ setImage}) {
   function handleTakePhoto (dataUri) {
     // Do stuff with the photo...
-    console.log('takePhoto2');
+    // console.log('takePhoto2');
     setImage(dataUri)
   }
 
   function handleTakePhotoAnimationDone (dataUri) {
     // Do stuff with the photo...
-    console.log('takePhoto1');
+    // console.log('takePhoto1');
   }
 
   function handleCameraError (error) {
@@ -27,23 +28,29 @@ function App({ setImage}) {
   }
 
   return (
-    <Camera
-      onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
-      onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
-      onCameraError = { (error) => { handleCameraError(error); } }
-      idealFacingMode = {FACING_MODES.ENVIRONMENT}
-      idealResolution = {{width: 640, height: 480}}
-      imageType = {IMAGE_TYPES.JPG}
-      imageCompression = {0.97}
-      isMaxResolution = {true}
-      isImageMirror = {false}
-      isSilentMode = {false}
-      isDisplayStartCameraError = {true}
-      isFullscreen = {false}
-      sizeFactor = {1}
-      onCameraStart = { (stream) => { handleCameraStart(stream); } }
-      onCameraStop = { () => { handleCameraStop(); } }
-    />
+    <Row justify="center">
+      <Col span={2}/>
+      <Col span={20}> 
+        <Camera
+          onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+          onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
+          onCameraError = { (error) => { handleCameraError(error); } }
+          idealFacingMode = {FACING_MODES.ENVIRONMENT}
+          idealResolution = {{width: 480, height: 480}}
+          imageType = {IMAGE_TYPES.JPG}
+          imageCompression = {1}
+          isMaxResolution = {true}
+          isImageMirror = {false}
+          isSilentMode = {false}
+          isDisplayStartCameraError = {true}
+          isFullscreen = {false}
+          sizeFactor = {1}
+          onCameraStart = { (stream) => { handleCameraStart(stream); } }
+          onCameraStop = { () => { handleCameraStop(); } }
+        />
+      </Col>
+      <Col span={2}/>
+    </Row>
   );
 }
 
