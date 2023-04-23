c748ba6c9ac31911e6c9b2fc96f13117641897f2
aefnattanon <aef.nattanon@gmail.com>
2023-04-23 22:44:57 +0700

    Update about

--- src/components/WebcamCapturePro.jsx
+++ src/components/WebcamCapturePro.jsx
@@ -2,47 +2,47 @@ import React from 'react';
 import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 
-function App({ setImage}) {
-  function handleTakePhoto (dataUri) {
+function App({ setImage }) {
+  function handleTakePhoto(dataUri) {
     // Do stuff with the photo...
     console.log('takePhoto2');
     setImage(dataUri)
   }
 
-  function handleTakePhotoAnimationDone (dataUri) {
+  function handleTakePhotoAnimationDone(dataUri) {
     // Do stuff with the photo...
     console.log('takePhoto1');
   }
 
-  function handleCameraError (error) {
+  function handleCameraError(error) {
     console.log('handleCameraError', error);
   }
 
-  function handleCameraStart (stream) {
+  function handleCameraStart(stream) {
     console.log('handleCameraStart');
   }
 
-  function handleCameraStop () {
+  function handleCameraStop() {
     console.log('handleCameraStop');
   }
 
   return (
     <Camera
-      onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
-      onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
-      onCameraError = { (error) => { handleCameraError(error); } }
-      idealFacingMode = {FACING_MODES.ENVIRONMENT}
-      idealResolution = {{width: 640, height: 480}}
-      imageType = {IMAGE_TYPES.JPG}
-      imageCompression = {0.97}
-      isMaxResolution = {true}
-      isImageMirror = {false}
-      isSilentMode = {false}
-      isDisplayStartCameraError = {true}
-      isFullscreen = {false}
-      sizeFactor = {1}
-      onCameraStart = { (stream) => { handleCameraStart(stream); } }
-      onCameraStop = { () => { handleCameraStop(); } }
+      onTakePhoto={(dataUri) => { handleTakePhoto(dataUri); }}
+      onTakePhotoAnimationDone={(dataUri) => { handleTakePhotoAnimationDone(dataUri); }}
+      onCameraError={(error) => { handleCameraError(error); }}
+      idealFacingMode={FACING_MODES.ENVIRONMENT}
+      idealResolution={{ width: 640, height: 640 }}
+      imageType={IMAGE_TYPES.JPG}
+      imageCompression={0.97}
+      isMaxResolution={true}
+      isImageMirror={false}
+      isSilentMode={false}
+      isDisplayStartCameraError={true}
+      isFullscreen={false}
+      sizeFactor={1}
+      onCameraStart={(stream) => { handleCameraStart(stream); }}
+      onCameraStop={() => { handleCameraStop(); }}
     />
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,21 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
+import { Col, Row } from 'antd';
 import MyModel from "../components/MyModel";
 import WebcamCapture from "../components/WebcamCapturePro";
 
-function About() {
+export default function About() {
 
   const [image, setImage] = useState(null);
   return (
-    <>
+    <section className="content-center max-h-screen items-center text-center">
       <h1>My demo</h1>
-      <WebcamCapture setImage={setImage} image={image} />
-      <MyModel  setImage={setImage} image={image} />
-    </>
+      <Row>
+        <Col span={4} />
+        <Col span={16}><WebcamCapture setImage={setImage} image={image} /></Col>
+        <Col span={4} />
+      </Row>
+      <MyModel setImage={setImage} image={image} />
+    </section >
   );
 }
-export default About;
+
